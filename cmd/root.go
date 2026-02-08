9d343976711a31cc6923483267c41cdfa25d9952
Javier Moya <jmoya@hey.com>
2026-02-08 08:54:38 +0100

    Reformat

--- cmd/root.go
+++ cmd/root.go
@@ -27,5 +27,15 @@ const helpTemplate = `{{with .Short}}{{.}}
 {{range .Commands}}{{if .IsAvailableCommand}}  {{.Name}}
 {{end}}{{end}}
 
-Run "{{.CommandPath}} <command> --help" for more information.
+` + "\x1b[1m" + `FLAGS` + "\x1b[0m" + `
+  --help      Show help for command
+  --version   Show odev version
+
+` + "\x1b[1m" + `EXAMPLES` + "\x1b[0m" + `
+  $ odev new nccl
+  $ odev examine
+
+` + "\x1b[1m" + `LEARN MORE` + "\x1b[0m" + `
+  Use ` + "`odev <command> <subcommand> --help`" + ` for more information about a command.
+  Read the manual at https://books.oreol.ch/6/cli
 `
