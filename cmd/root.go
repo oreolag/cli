e316331a89251bc2a9db11d5f960cd88a6a0df87
Javier Moya <jmoya@hey.com>
2026-02-08 08:21:41 +0100

    Update root

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,23 @@ package cmd
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use: "odev",
+	Use:   "odev",
+	Short: "Work seamlessly with heterogeneous systems from the command line.",
 }
 
 func Execute() {
+	rootCmd.SetHelpTemplate(helpTemplate)
 	_ = rootCmd.Execute()
 }
+
+const helpTemplate = `{{with .Short}}{{.}}
+
+{{end}}USAGE
+  {{.CommandPath}} <command> [flags]
+
+COMMANDS
+{{range .Commands}}{{if (and .IsAvailableCommand (not .IsHelpCommand))}}
+  {{rpad .Name .NamePadding }}{{.Short}}{{end}}{{end}}
+
+Run "{{.CommandPath}} <command> --help" for more information.
+`
