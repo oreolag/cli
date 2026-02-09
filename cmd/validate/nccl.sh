54b6a3e5a1a6d70b9e0861ab8c355161027d0391
Javier Moya <jmoya@hey.com>
2026-02-09 11:55:56 +0100

    Working version

--- cmd/validate/nccl.sh
+++ cmd/validate/nccl.sh
@@ -8,4 +8,12 @@
 #usage:       $CLI_PATH/hdev validate opennic --commit $commit_name_shell $commit_name_driver --device $device_index --fec $fec_option --version $vivado_version
 #example: /opt/hdev/cli/hdev validate opennic --commit            8077751             1cf2578 --device             1 --fec 1           --version          2022.2
 
-echo "Hey I am here!"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ODEV_ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+
+# constants
+TMP_PATH="$("$ODEV_ROOT/src/constants_get.py" --db "$ODEV_ROOT/src/constants.yml" paths tmp)"
+
+echo $TMP_PATH
+
+# create folders
