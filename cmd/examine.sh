496e529cf05f34dd52322c3d38d47e084a22bced
Javier Moya <jmoya@hey.com>
2026-02-10 15:58:42 +0100

    Calling examine works

---
+++ cmd/examine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#ODEV_ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+ODEV_ROOT="${ODEV_ROOT:-"$(dirname "$SCRIPT_DIR")"}"
+WORKFLOW="$(basename "${BASH_SOURCE[0]}" .sh)"
+
+# usage:       $CLI_PATH/hdev validate opennic --commit $commit_name_shell $commit_name_driver --device $device_index --fec $fec_option --version $vivado_version
+# example: /opt/hdev/cli/hdev validate opennic --commit            8077751             1cf2578 --device             1 --fec 1           --version          2022.2
+
+# early exit
+url="${HOSTNAME}"
+hostname="${url%%.*}"
+
+echo "hola"
