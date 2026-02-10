c6131bd6c376e8f8c42a01323bfe3cff199b5b2e
Javier Moya <jmoya@hey.com>
2026-02-10 12:51:16 +0100

    Works for bash

--- cmd/validate/nccl.sh
+++ cmd/validate/nccl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ODEV_ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+#ODEV_ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+ODEV_ROOT="${ODEV_ROOT:-"$(dirname "$SCRIPT_DIR")"}"
 WORKFLOW="$(basename "${BASH_SOURCE[0]}" .sh)"
 
 # usage:       $CLI_PATH/hdev validate opennic --commit $commit_name_shell $commit_name_driver --device $device_index --fec $fec_option --version $vivado_version
@@ -12,8 +13,8 @@ url="${HOSTNAME}"
 hostname="${url%%.*}"
 
 # constants
-CMDB_PATH="$(eval echo "$("$ODEV_ROOT/src/read_yml.py" --db "$ODEV_ROOT/src/constants.yml" paths cmdb)")"
-PROJECTS_PATH="$(eval echo "$("$ODEV_ROOT/src/read_yml.py" --db "$ODEV_ROOT/src/constants.yml" paths projects)")"
+CMDB_PATH="$(eval echo "$("$ODEV_ROOT/src/read_yml.py" --db "$ODEV_ROOT/constants.yml" paths cmdb)")"
+PROJECTS_PATH="$(eval echo "$("$ODEV_ROOT/src/read_yml.py" --db "$ODEV_ROOT/constants.yml" paths projects)")"
 VALIDATION_PROJECT_PATH="$PROJECTS_PATH/validate.$WORKFLOW.$hostname"
 
 # derived
@@ -24,7 +25,7 @@ rm -rf "$VALIDATION_PROJECT_PATH"
 mkdir -p "$VALIDATION_PROJECT_PATH"
 
 # copy files from template
-cp -r "$ODEV_ROOT/src/templates/nvidia/nccl-tests/." "$VALIDATION_PROJECT_PATH"
+cp -r "$ODEV_ROOT/templates/nvidia/nccl-tests/." "$VALIDATION_PROJECT_PATH"
 
 # build tets (for local tests, MPI flag is not needed)
 cd "$VALIDATION_PROJECT_PATH"
