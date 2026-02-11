a98d4a7d02abbb1bdc43242acd1df4950a30a61c
Javier Moya <jmoya@hey.com>
2026-02-11 13:30:37 +0100

    Ad flags

--- src/cmd_flags.sh
+++ src/cmd_flags.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+# run
+# nccl
+# ...
+
+# validate
+# nccl
 VALIDATE_NCCL_DESCRIPTION="Validate NCCL"
 VALIDATE_NCCL_FLAGS=(
-  "ngpus,g,Number of GPUs,1-10,1"
-  "nthreads,t,Number of Threads,1-1024,10"
-  "minbytes,b,Minimum Bytes,1B-1G,8M"
-  "maxbytes,e,Maximum Bytes,1B-16G,100M"
+  "ngpus,g,Number of GPUs,1-8,1"
+  "nthreads,t,Threads per process,1-64,1"
+  "minbytes,b,Minimum message size,1B-1G,8M"
+  "maxbytes,e,Maximum message size,1B-16G,1G"
+  "iters,n,Timed iterations,1-1000,20"
+  "datatype,d,Specify which datatype to use,int8|half|bfloat16|float,float"
+)
+VALIDATE_NCCL_FLAGS_MANDATORY=(
+  "ngpus"
+  "maxbytes"
 )
