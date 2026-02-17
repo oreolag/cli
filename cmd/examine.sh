95c8d37e8cb0527965b309698e2203618e56ef54
Javier Moya <jmoya@hey.com>
2026-02-17 14:34:48 +0100

    Working version

--- cmd/examine.sh
+++ cmd/examine.sh
@@ -18,7 +18,10 @@ bold=$(tput bold)
 italic=$(tput sitm 2>/dev/null || true)
 normal=$(tput sgr0)
 
-#print operating system information
+# constants
+TMP_PATH="$(eval echo "$("$ODEV_PATH/src/read_yml.py" --db "$ODEV_PATH/constants.yml" paths tmp)")"
+
+# print operating system information
 . /etc/os-release
 echo "${bold}${NAME} ${VERSION}${normal}"
 description=$(lsb_release -d | awk -F'\t' '{print $2}' | sed 's/^[^0-9]*//')
@@ -29,3 +32,26 @@ echo "Description : ${bold}$description${normal}"
 echo "Codename    : ${bold}$codename${normal}"
 echo "Linux kernel: ${bold}$linux_kernel${normal}"
 echo "Uptime      : ${bold}$uptime_info${normal}"
+
+# lstopo
+rm -rf $TMP_PATH/lstopo_output
+lstopo-no-graphics 2>/dev/null > $TMP_PATH/lstopo_output
+
+# get total memory
+total_memory=$(awk -F'[()]' '/^Machine/ {print $2}' "$TMP_PATH/lstopo_output" | awk '{print $1}')
+
+# lscpu
+model_name=$(lscpu | awk -F: '/Model name/ {print $2}' | xargs)
+[[ "$model_name" =~ Cortex- ]] && model_name=$(echo "$model_name" | awk '{print $1}')
+cpu_count=$(lscpu | grep -i "^CPU(s):" | awk '{print $2}')
+online_cpus=$(lscpu | grep -i "On-line CPU(s) list" | awk -F: '{print $2}' | xargs)
+
+echo ""
+echo "${bold}$model_name${normal}"
+echo "CPU(s): $cpu_count ($online_cpus)"
+echo "Total memory: $total_memory"
+
+# lstopo loop 
+numa_nodes=$(lscpu | grep -i "NUMA node(s)" | awk '{print $NF}')
+
+echo $numa_nodes
