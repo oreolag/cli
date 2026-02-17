df46caaa8441ad3ec355ea7b1ba62684d78988fe
Javier Moya <jmoya@hey.com>
2026-02-17 13:52:53 +0100

    Working version

--- cmd/examine.sh
+++ cmd/examine.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ODEV_PATH="${ODEV_PATH:-"$(dirname "$SCRIPT_DIR")"}"
-WORKFLOW="$(basename "${BASH_SOURCE[0]}" .sh)"
-
-# usage:       $CLI_PATH/hdev validate opennic --commit $commit_name_shell $commit_name_driver --device $device_index --fec $fec_option --version $vivado_version
-# example: /opt/hdev/cli/hdev validate opennic --commit            8077751             1cf2578 --device             1 --fec 1           --version          2022.2
+# example: odev examine
 
 # early exit
 url="${HOSTNAME}"
 hostname="${url%%.*}"
 
-echo "hola"
+# get script location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WORKFLOW="$(basename "${BASH_SOURCE[0]}" .sh)"
+
+# derive from SCRIPT_DIR
+ODEV_PATH="${ODEV_PATH:-"$(dirname "$SCRIPT_DIR")"}"
+
+# format
+bold=$(tput bold)
+italic=$(tput sitm 2>/dev/null || true)
+normal=$(tput sgr0)
+
+#print operating system information
+. /etc/os-release
+echo "${bold}${NAME} ${VERSION}${normal}"
+description=$(lsb_release -d | awk -F'\t' '{print $2}' | sed 's/^[^0-9]*//')
+codename=$(lsb_release -c | awk -F':' '{print $2}' | xargs)
+linux_kernel=$(uname -r)
+uptime_info=$(uptime -p)
+echo "Description : ${bold}$description${normal}"
+echo "Codename    : ${bold}$codename${normal}"
+echo "Linux kernel: ${bold}$linux_kernel${normal}"
+echo "Uptime      : ${bold}$uptime_info${normal}"
