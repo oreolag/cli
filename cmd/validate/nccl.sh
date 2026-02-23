8f7fd0063c83fbeec7a6b2cfb8209cdaa8d8f984
Javier Moya <jmoya@hey.com>
2026-02-23 20:23:23 +0100

    Working version

--- cmd/validate/nccl.sh
+++ cmd/validate/nccl.sh
@@ -11,26 +11,32 @@ CLI_NAME="$(basename "$(dirname "$(dirname "$SCRIPT_DIR")")")"
 COMMAND="$(basename "$SCRIPT_DIR")"
 ODEV_PATH="${ODEV_PATH:-"$(dirname "$SCRIPT_DIR")"}"
 
-# read command flags
+# read command description
 KEY="$(printf '%s_%s' "$COMMAND" "$WORKFLOW" | tr '[:lower:]' '[:upper:]')"
-result="$("$ODEV_PATH/src/cmd_flags_read.sh" "$ODEV_PATH" "$KEY")"
-COMMAND_DESCRIPTION="$(echo "$result" | sed -n 's/^DESCRIPTION=//p' | head -n1)"
-FLAGS=()
-while IFS= read -r line; do
-  FLAGS+=("$line")
-done < <(echo "$result" | sed -n 's/^FLAG=//p')
+command_description="$("$ODEV_PATH/src/cmd_description_read.sh" "$ODEV_PATH" "$KEY")"
+
+# read command flags
+mapfile -t flags < <("$ODEV_PATH/src/cmd_flags_read.sh" "$ODEV_PATH" "$KEY")
+#result="$("$ODEV_PATH/src/cmd_flags_read.sh" "$ODEV_PATH" "$KEY")"
+#COMMAND_DESCRIPTION="$(echo "$result" | sed -n 's/^DESCRIPTION=//p' | head -n1)"
+#FLAGS=()
+#while IFS= read -r line; do
+#  FLAGS+=("$line")
+#done < <(echo "$result" | sed -n 's/^FLAG=//p')
+
+
 
 # (maybe) print help
 print_range="1"
 print_default="0"
 print_both="0"
 "$ODEV_PATH/src/cmd_help_print.sh" --maybe \
-  "$CLI_NAME" "$COMMAND" "$WORKFLOW" "$COMMAND_DESCRIPTION" \
+  "$CLI_NAME" "$COMMAND" "$WORKFLOW" "$command_description" \
   "$print_range" "$print_default" "$print_both" \
-  "${FLAGS[@]}" -- "$@" && exit 0 || true
+  "${flags[@]}" -- "$@" && exit 0 || true
 
 # parse and check flag values
-parsed_flags="$("$ODEV_PATH/src/cmd_parse.sh" --params "${FLAGS[@]}" -- "$@")" || exit 1
+parsed_flags="$("$ODEV_PATH/src/cmd_parse.sh" --params "${flags[@]}" -- "$@")" || exit 1
 if [[ -n "$parsed_flags" ]]; then
   declare -A V
   while IFS='=' read -r k v; do
