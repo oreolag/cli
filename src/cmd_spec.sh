66f40b148f65c080292a5aa0a772bfb256794176
Javier Moya <jmoya@hey.com>
2026-03-09 14:02:12 +0100

    Properly sources workflows

--- src/cmd_spec.sh
+++ src/cmd_spec.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # examine 
 EXAMINE_DESCRIPTION="System and device information"
 EXAMINE_FLAGS=(
@@ -9,12 +11,7 @@ EXAMINE_FLAGS=(
 NEW_DESCRIPTION="Create a new workflow or project based on the selected template"
 NEW_FLAGS=(
 )
-# vllm
-NEW_VLLM_DESCRIPTION="Creates a new project configured for vLLM"
-NEW_VLLM_FLAGS=(
-  "name,n,Project name,-,-"
-  "framework,f,Framework to be used,-,-"
-)
+# workflow
 NEW_WORKFLOW_DESCRIPTION="Create a new accelerated workflow"
 NEW_WORKFLOW_FLAGS=(
   "name,n,Workflow name,-,-"
@@ -30,14 +27,17 @@ VALIDATE_DESCRIPTION="Infrastructure functionality assessment"
 VALIDATE_FLAGS=(
 )
 # nccl
-VALIDATE_NCCL_DESCRIPTION="NVIDIA Collective Communications Library (NCCL) validation"
-VALIDATE_NCCL_FLAGS=(
-  "ngpus,g,Number of GPUs,1-8,1"
-  "nthreads,t,Threads per process,1-64,1"
-  "minbytes,b,Minimum message size,1B|4K|8M|1G,8M"
-  "maxbytes,e,Maximum message size,1B|4K|1G|16G,1G"
-  "iters,n,Timed iterations,1-1000,20"
-  "datatype,d,Specify which datatype to use,int8|half|bfloat16|float,float"
-  "stepfactor,f,Multiplication factor between sizes,2|4|8,2"
-)
-VALIDATE_NCCL_FLAGS_MANDATORY="ngpus,minbytes,maxbytes"
+#VALIDATE_NCCL_DESCRIPTION="NVIDIA Collective Communications Library (NCCL) validation"
+#VALIDATE_NCCL_FLAGS=(
+#  "ngpus,g,Number of GPUs,1-8,1"
+#  "nthreads,t,Threads per process,1-64,1"
+#  "minbytes,b,Minimum message size,1B|4K|8M|1G,8M"
+#  "maxbytes,e,Maximum message size,1B|4K|1G|16G,1G"
+#  "iters,n,Timed iterations,1-1000,20"
+#  "datatype,d,Specify which datatype to use,int8|half|bfloat16|float,float"
+#  "stepfactor,f,Multiplication factor between sizes,2|4|8,2"
+#)
+#VALIDATE_NCCL_FLAGS_MANDATORY="ngpus,minbytes,maxbytes"
+
+# workflows
+source "$SCRIPT_DIR/../submodules/workflows/nccl/cmd_spec.sh"
