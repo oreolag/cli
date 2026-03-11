e4a6796b6be85890ffb14a0524cc88dc35e370f7
Javier Moya <jmoya@hey.com>
2026-03-11 19:12:34 +0100

    First commit

---
+++ src/required_tools_print.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ODEV_PATH="$1"
+tool="${2:-}"
+
+mapfile -t required_tools < "$ODEV_PATH/src/required_tools"
+
+if [[ -z "$tool" ]]; then
+  for t in "${required_tools[@]}"; do
+    installed="$("$ODEV_PATH/src/which.sh" "$t")"
+    if [[ "$installed" == "0" ]]; then
+      echo "👎 $t"
+    else
+      echo "👍🏻 $t"
+    fi
+  done
+else
+  installed="$("$ODEV_PATH/src/which.sh" "$tool")"
+  echo "$installed"
+fi
