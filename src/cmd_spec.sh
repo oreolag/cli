38b7a16d7e466d19dc245a51bc046fee158ee6ef
Javier Moya <jmoya@hey.com>
2026-03-17 10:58:52 +0100

    Delete project replaced

--- src/cmd_spec.sh
+++ src/cmd_spec.sh
@@ -25,13 +25,6 @@ DELETE_DESCRIPTION="Deletes an artifact of your choice"
 DELETE_FLAGS=(
 )
 # workflow
-DELETE_PROJECT_DESCRIPTION="Deletes an existing user project"
-DELETE_PROJECT_FLAGS=(
-  "project,p,Project name,-,-"
-  "workflow,w,Workflow name,-,-"
-)
-DELETE_PROJECT_FLAGS_MANDATORY="project,workflow"
-# workflow
 DELETE_WORKFLOW_DESCRIPTION="Deletes an existing user workflow"
 DELETE_WORKFLOW_FLAGS=(
   "name,n,Workflow name,-,-"
