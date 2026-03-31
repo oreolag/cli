5f4f7927e400c4e4b8b3d0312702081e3cba2787
Javier Moya <jmoya@hey.com>
2026-03-31 18:02:25 +0200

    Cleanup

--- cmd/get/ip.sh
+++ cmd/get/ip.sh
@@ -22,13 +22,6 @@ normal=$(tput sgr0)
 
 # constants
 CMDB_PATH="$(eval echo "$("$ODEV_PATH/src/read_yml.py" --db "$ODEV_PATH/constants.yml" paths cmdb)")"
-#COLOR_NIC=$($ODEV_PATH/src/color_get.sh $ODEV_PATH COLOR_NIC)
-#COLOR_NVIDIA=$($ODEV_PATH/src/color_get.sh $ODEV_PATH COLOR_NVIDIA)
-#COLOR_XILINX=$($ODEV_PATH/src/color_get.sh $ODEV_PATH COLOR_XILINX)
-#STORAGE_UNIT="TB"
-#STRING_ACCEL="Adaptive devices"
-#STRING_GPUS="GPUs"
-#STRING_NICS="NICs"
 TMP_PATH="$(eval echo "$("$ODEV_PATH/src/read_yml.py" --db "$ODEV_PATH/constants.yml" paths tmp)")"
 
 # check on users
--- cmd/ifconfig.sh
+++ cmd/ifconfig.sh
@@ -25,10 +25,6 @@ CMDB_PATH="$(eval echo "$("$ODEV_PATH/src/read_yml.py" --db "$ODEV_PATH/constant
 COLOR_NIC=$($ODEV_PATH/src/color_get.sh $ODEV_PATH COLOR_NIC)
 COLOR_NVIDIA=$($ODEV_PATH/src/color_get.sh $ODEV_PATH COLOR_NVIDIA)
 COLOR_XILINX=$($ODEV_PATH/src/color_get.sh $ODEV_PATH COLOR_XILINX)
-#STORAGE_UNIT="TB"
-#STRING_ACCEL="Adaptive devices"
-#STRING_GPUS="GPUs"
-#STRING_NICS="NICs"
 TMP_PATH="$(eval echo "$("$ODEV_PATH/src/read_yml.py" --db "$ODEV_PATH/constants.yml" paths tmp)")"
 
 # set KEY
