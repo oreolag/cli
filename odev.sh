74f402eec7cfba880cecc8f4f36c1d4b5f43b106
Javier Moya <jmoya@hey.com>
2026-04-02 08:01:28 +0200

    Adding SERVER STATUS

--- odev.sh
+++ odev.sh
@@ -14,6 +14,9 @@ bold=$(tput bold)
 italic=$(tput sitm 2>/dev/null || true)
 normal=$(tput sgr0)
 
+# constants
+CMDB_PATH="$(eval echo "$("$ODEV_PATH/src/read_yml.py" --db "$ODEV_PATH/constants.yml" paths cmdb)")"
+
 # command descriptions
 build="$("$ODEV_PATH/src/cmd_description_read.sh" "$ODEV_PATH" "BUILD")"
 get="$("$ODEV_PATH/src/cmd_description_read.sh" "$ODEV_PATH" "GET")"
@@ -25,16 +28,19 @@ validate="$("$ODEV_PATH/src/cmd_description_read.sh" "$ODEV_PATH" "VALIDATE")"
 examine="$("$ODEV_PATH/src/cmd_description_read.sh" "$ODEV_PATH" "EXAMINE")"
 
 # check on GitHub CLI
-#installed="$("$ODEV_PATH/src/required_tools_print.sh" "$ODEV_PATH" "gh")"
-#if [[ "$installed" == "1" ]]; then
-#  logged_in="$("$ODEV_PATH/src/gh_auth_status.sh")"
-#  if [[ "$logged_in" == "1" ]]; then
-#    github_user="$(gh api user --jq .login)"
-#    gh_status="You are logged in to GitHub CLI as ${bold}$github_user${normal}"
-#  else
-#    gh_status="Please use login to GitHub CLI with ${bold}gh auth login${normal}"
-#  fi
-#fi
+installed="$("$ODEV_PATH/src/required_tools_print.sh" "$ODEV_PATH" "gh")"
+if [[ "$installed" == "1" ]]; then
+  logged_in="$("$ODEV_PATH/src/gh_auth_status.sh")"
+  if [[ "$logged_in" == "1" ]]; then
+    github_user="$(gh api user --jq .login)"
+    gh_status="You are logged in to GitHub CLI as ${bold}$github_user${normal}"
+  else
+    gh_status="Please use login to GitHub CLI with ${bold}gh auth login${normal}"
+  fi
+fi
+
+# check on build
+is_build=$($ODEV_PATH/src/is_server.sh "$CMDB_PATH" "build")
 
 print_help() {
   echo "The CLI for hetero${italic}genius${normal} computing."
@@ -61,6 +67,16 @@ print_help() {
   echo "${bold}EXAMPLES${normal}"
   echo "  $ odev examine"
   echo "  $ odev validate nccl"
+  echo ""
+  echo "${bold}SERVER STATUS${normal}"
+  if [ "$is_build" = "0" ]; then
+    echo "  This is a ${bold}deployment${normal} server"
+  else
+    echo "  This is a ${bold}development${normal} server"
+  fi
+  if [ ! "$gh_status" = "" ]; then
+    echo "  $gh_status"
+  fi
   echo ""
   echo "${bold}LEARN MORE${normal}"
   echo "  Use ${bold}odev <command> <subcommand> --help${normal} for more information about a command."
