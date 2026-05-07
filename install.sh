7cdbefb68f21cab75ad6fa1622ff583f41fdde41
Javier Moya <jmoya@hey.com>
2026-05-07 06:44:52 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+REPO_URL="https://github.com/oreolag/cli.git"
+TMP_PATH="$(mktemp -d)"
+
+cleanup() {
+    rm -rf "$TMP_PATH"
+}
+
+trap cleanup EXIT
+
+# root check
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Please run as root:"
+    echo "curl -fsSL https://raw.githubusercontent.com/oreolag/cli/main/install.sh | sudo bash"
+    exit 1
+fi
+
+echo "Hey I am here"
+exit 
+
+echo "[INFO] Installing prerequisites..."
+
+apt-get update
+
+apt-get install -y \
+    git \
+    ansible \
+    python3 \
+    python3-pip \
+    rsync \
+    sudo
+
+echo "[INFO] Cloning odev repository..."
+
+git clone --recursive "$REPO_URL" "$TMP_PATH/cli"
+
+cd "$TMP_PATH/cli"
+
+echo "[INFO] Running installer..."
+
+ansible-playbook \
+    -i localhost, \
+    -c local \
+    install.yml \
+    --extra-vars "repo=true"
+
+echo
+echo "[INFO] odev installation completed"
+echo
+echo "Try:"
+echo "  odev --help"
