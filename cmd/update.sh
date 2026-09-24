5cf5fc83254571e3bd15c8080acd6cbe6099501c
Javier Moya <javier.moya@icloud.com>
2026-09-24 09:50:24 +0200

    Fix empty $tool

--- cmd/update.sh
+++ cmd/update.sh
@@ -37,7 +37,7 @@ fi
 # check on tools
 installed="$("$ODEV_PATH/src/required_tools_print.sh" "$ODEV_PATH" "gh")"
 if [[ "$installed" == "0" ]]; then
-  echo "Missing tool: $tool"
+  echo "Missing tool: gh"
   exit 1
 fi
 
